16b28b1f0a79cb0a87c85f07684b25d2699b07d0
Wildan S. Nahar <wildansn@arc.itb.ac.id>
2016-10-09 19:25:46 +0700

    variable, comment, and regulations

--- src/learning/hello.go
+++ src/learning/hello.go
@@ -5,8 +5,23 @@ func main()  {
   // variable declaration
   var firstName string = "Wildan"
   var lastName = "Syahrun"
+  middleName := "Nahar"
+
+  // multiple var declaration
+  var first, second, third string
+  first, second, third = "satu", "dua", "tiga"
+
+  // multiple types of vars
+  nama, umur, lahir, cowok := "WSN", 20, 1994, true
+  var freeVar = "freelancer"
+
+  // trash var
+  _ = "learning GO"
+
+  fmt.Printf("BIODATA : %s %s %s %s", nama, umur, lahir, cowok, freeVar)
+  fmt.Printf("This is an example : %s %s %s \n", first, second, third)
 
   fmt.Printf("halo Wildan Syahrun! \n")
-  fmt.Printf("halo %s %s!\n", firstName, lastName)
+  fmt.Printf("halo %s %s %s!\n", firstName, lastName, middleName)
   fmt.Println("halo", firstName, lastName + "!")
 }
