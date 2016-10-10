ff9fed7af579d9671a8bc7a41acb4700a7dce98b
Wildan S. Nahar <wildansnahar@gmail.com>
2016-10-10 21:03:45 +0700

    array

---
+++ src/learning/array.go
@@ -0,0 +1,29 @@
+package main
+import "fmt"
+
+func main()  {
+  var names [4] string
+  names[0] = "andy"
+  names[1] = "yandi"
+  names[2] = "lukman"
+  names[3] = "ardi"
+
+  fmt.Println(names[0], names[1], names[2], names[3])
+
+  // direct declaration
+  var fruits = [4] string {"apple","pinneapple","strawberry","orange"}
+
+  fmt.Println("Total fruit \t \t", len(fruits))
+  fmt.Println("Each fruit : \t ", fruits)
+
+  // initialization without prefix value
+  var numbers = [...] int{2,3,4,45,45}
+  fmt.Println(numbers)
+
+  // multidimension array
+  var numbers1 = [2][3]int{[3]int{1,2,3}, [3]int{4,5,6}}
+  var numbers2 = [2][2]int{[2]int{1,2}, [2]int{4,5}}
+
+  fmt.Println("numbers1 ", numbers1)
+  fmt.Println("numbers2 ", numbers2)
+}
