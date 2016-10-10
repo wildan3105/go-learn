1c7f54fb8a584b36a556193213737aa5597c0313
Wildan S. Nahar <wildansn@arc.itb.ac.id>
2016-10-10 19:34:01 +0700

    datatype

---
+++ src/learning/datatype_const_opr.go
@@ -0,0 +1,29 @@
+package main
+import "fmt"
+
+func main()  {
+  name:= new(string)
+
+  fmt.Println(name)
+  fmt.Println(*name)
+
+  /*
+  variable declaration using make
+  only valid for these 3 vars : channel, slice, map
+  */
+  // numeric non-decimal variable
+  var positiveNumb uint8 = 99
+  var negativeNumb = -1212
+
+  fmt.Printf("positive numb : %d \n", positiveNumb)
+  fmt.Printf("negative numb : %d \n", negativeNumb)
+
+  // bilangan cacah
+  var decimalNumb = 2.62
+  fmt.Printf("decimal numb : %f \n", decimalNumb)
+  fmt.Printf("decimal numb : %.3f \n", decimalNumb)
+
+  // boolean var
+  var exist bool = true
+  fmt.Printf("exist ? %t \n", exist)
+}
