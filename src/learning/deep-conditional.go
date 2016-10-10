90de58103a163167f7f1fa4b3f0449819a365d8a
Wildan S. Nahar <wildansn@arc.itb.ac.id>
2016-10-10 20:26:54 +0700

    deep conditional

---
+++ src/learning/deep-conditional.go
@@ -0,0 +1,17 @@
+package main
+import "fmt"
+
+func main()  {
+  var point = 6
+  switch {
+  case point == 6:
+    fmt.Println("perfect")
+  case (point < 8) && (point > 3):
+    fmt.Println("awesome")
+  default:
+    {
+      fmt.Println("not bad")
+      fmt.Println("you need to learn more")
+    }
+  }
+}
