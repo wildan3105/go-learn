3c06151bf6058e20d3ab9a433e46f2afce4c7a6c
Wildan S. Nahar <wildansnahar@gmail.com>
2016-10-12 19:54:09 +0700

    function

--- src/exercise/7-functions.go
+++ src/exercise/7-functions.go
@@ -1,13 +1,27 @@
 package main
 import "fmt"
 
-func main()  {
-  xs := []float64{98,93,77,82,83}
-
+func average(xs []float64) float64 {
   total := 0.0
-  for _, v := range xs {
+
+  for _, v:= range xs {
     total += v
   }
-  fmt.Println("test")
-  fmt.Println(total / float64(len(xs)))
+  return total / float64(len(xs))
+}
+
+func main()  {
+  xs := []float64{10, 20, 30, 40}
+  fmt.Println(average(xs))
+
+  // another example
+  fmt.Println(f1)
+}
+
+func f1() int {
+  return f2()
+}
+
+func f2() int {
+  return 20
 }
