fecdb0d1752684d4fc74f4c266bddcab5f8257da
Wildan S. Nahar <wildansnahar@gmail.com>
2016-10-16 06:55:39 +0700

    TCP server

---
+++ src/exercise/13.7-tcp.go
@@ -0,0 +1,62 @@
+package main
+import (
+  "encoding/gob"
+  "fmt"
+  "net"
+)
+
+func server()  {
+  // listen on a port
+  ln, err := net.Listen("tcp", ":9999")
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+  for {
+    // accept a connection
+    c, err := ln.Accept()
+    if err != nil {
+      fmt.Println(err)
+      continue
+    }
+    // handle the connection
+    go handdleServerConnection(c)
+  }
+}
+
+func handdleServerConnection(c net.Conn){
+  // receive the message
+  var msg string
+  err := gob.NewDecoder(c).Decode(&msg)
+  if err != nil {
+    fmt.Println(err)
+  } else {
+    fmt.Println("message", msg)
+  }
+  c.Close()
+}
+
+func client(){
+  // connect to the server
+  c, err := net.Dial("tcp", "127.0.0.1:9999")
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+
+  // send the message
+  msg := "hello world"
+  fmt.Println("Sending", msg)
+  err = gob.NewEncoder(c).Encode(msg)
+  if err != nil {
+    fmt.Println(err)
+  }
+  c.Close()
+}
+
+func main(){
+  go server()
+  go client()
+  var input string
+  fmt.Scanln(&input)
+}
