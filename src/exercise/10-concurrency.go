ed4b2f57ac516f1b9172ea756b1de59bb7766887
Wildan S. Nahar <wildansnahar@gmail.com>
2016-10-15 20:38:38 +0700

    goroutines

--- src/exercise/10-concurrency.go
+++ src/exercise/10-concurrency.go
@@ -1,14 +1,25 @@
 package main
-import "fmt"
+import (
+  "fmt"
+  "time"
+  "math/rand"
+)
+
+
+// give some delay
 
 func f(n int){
   for i:=0; i<10; i++{
     fmt.Println(n, ":", i)
+    amt := time.Duration(rand.Intn(1250))
+    time.Sleep(time.Millisecond * amt)
   }
 }
 
 func main()  {
-  go f(0)
+  for i:=0; i<10; i++ {
+    go f(i)
+  }
   var input string
   fmt.Scanln(&input)
 }
